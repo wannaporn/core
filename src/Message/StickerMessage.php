2bf0c8f87ebb9d4b9efd3921bafdfa29e52b2984
WATCHDOGS <taned@intbizth.com>
2017-08-24 22:56:07 +0700

    Update StickerMessage.php

--- src/Message/StickerMessage.php
+++ src/Message/StickerMessage.php
@@ -1,10 +1,22 @@
 <?php
 
+/*
+ * This file is part of the PhpMob package.
+ *
+ * (c) Ishmael Doss <nukboon@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace LineMob\Core\Message;
 
 use LineMob\Core\Command\AbstractCommand;
 use LineMob\Core\Template\StickerTemplate;
 
+/**
+* @author Taned Suparpornhemin <taned@intbizth.com>
+*/
 class StickerMessage extends AbstractMessage
 {
     /**
