df1c826771912f9b1386a2c96d872d81306142d7
WATCHDOGS <taned@intbizth.com>
2017-08-25 12:25:45 +0700

    more copyright

--- src/QuickStart.php
+++ src/QuickStart.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the LineMob package.
+ *
+ * (c) Ishmael Doss <nukboon@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace LineMob\Core;
 
 use League\Tactician\CommandBus;
@@ -15,6 +24,9 @@ use LineMob\Core\Message\ImageMessage;
 use LineMob\Core\Message\TextMessage;
 use LineMob\Core\Message\VideoMessage;
 
+/**
+ * @author Ishmael Doss <nukboon@gmail.com>
+ */
 class QuickStart
 {
     /**
