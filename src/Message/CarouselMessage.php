d35010e880fcb2115b6cb82b53492f29b7708288
WATCHDOGS <taned@intbizth.com>
2017-08-25 12:24:19 +0700

    Add copyright already have files

--- src/Message/CarouselMessage.php
+++ src/Message/CarouselMessage.php
@@ -1,10 +1,23 @@
 <?php
 
+/*
+ * This file is part of the LineMob package.
+ *
+ * (c) Ishmael Doss <nukboon@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 namespace LineMob\Core\Message;
 
 use LineMob\Core\Command\AbstractCommand;
 use LineMob\Core\Template\Carousel\CarouselTemplate;
 
+/**
+ * @author Ishmael Doss <nukboon@gmail.com>
+ */
 class CarouselMessage extends AbstractMessage
 {
     /**
