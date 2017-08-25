54d1d1c7e10b1d9e4c771184bdac2fdbedd95f1f
WATCHDOGS <taned@intbizth.com>
2017-08-25 12:29:38 +0700

    Add copyright

--- src/Template/LocationTemplate.php
+++ src/Template/LocationTemplate.php
@@ -1,9 +1,21 @@
 <?php
 
+/*
+ * This file is part of the LineMob package.
+ *
+ * (c) Ishmael Doss <nukboon@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace LineMob\Core\Template;
 
 use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
 
+/**
+ * @author WATCHDOGS <godoakbrutal@gmail.com>
+ */
 class LocationTemplate extends AbstractTemplate
 {
     /**
